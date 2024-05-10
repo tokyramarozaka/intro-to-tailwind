1c01f7acabde608d708cdedf0f6bd0ae89ed2127
Tokimahery Ramarozaka <tokyramarozaka@gmail.com>
2024-05-10 14:51:46 +0300

    feat: add tailwind basic project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#007bff',
+        secondary: '#6c757d',
+      },
+    },
+  },
+  plugins: [],
+}
+
